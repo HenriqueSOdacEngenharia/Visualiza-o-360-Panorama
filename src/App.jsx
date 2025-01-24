ddae81e115798f57a76b2266fee0af3ee8f4d5ab
Henrique <henriquesoliveira.dacengenharia@gmail.com>
2025-01-24 10:35:19 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPhotoSphereViewer
+        src="frame_0009.jpg"
+        height={"100vh"}
+        width={"100%"}
+      ></ReactPhotoSphereViewer>
+    </div>
+  );
+}
+
+export default App;
